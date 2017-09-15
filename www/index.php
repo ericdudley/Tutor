ce28be2f65cb64e74b50b0853023430891912e30
Eric Dudley <ericdudley@email.com>
2017-09-14 21:38:02 -0400

    Graphical updates.

--- www/index.php
+++ www/index.php
@@ -4,12 +4,12 @@
     <?php
     $db = SQLiteConnection::connect();
     if ($db != null) {
-        foreach(Config::CREATE_TABLE_STRINGS as $str) {
-           $db->exec($str);
+        foreach (Config::CREATE_TABLE_STRINGS as $str) {
+            $db->exec($str);
         }
     }
 
-    if(!file_exists('profile_pics/'.$_SERVER['uid'].'.jpg')){
+    if (!file_exists('profile_pics/' . $_SERVER['uid'] . '.jpg')){
         echo '<div class="pure-u-1 card">
 <h2>You don\'t have a profile pic, upload one!</h2>
 <form class="pure-form" action="upload-profile-pic.php" method="post" enctype="multipart/form-data">
@@ -17,23 +17,24 @@
     <input type="submit" name="submit" value="Upload">
 </form>
 </div>';
-    } else{
+    } else {
         echo '<div class="pure-u-1 card">
-<h2>Logged in as {$_SERVER["uid"]}</h2>
-<img src="profile_pics/{$_SERVER["uid"]}.jpg">
+<h2>Logged in as '.$_SERVER["uid"].'</h2>
+<img src="profile_pics/'.$_SERVER["uid"].'.jpg">
 </div>';
+    }
     ?>
     <div class="pure-u-md-5-24"></div>
-    <div class="pure-u-1 pure-u-md-1-4 card">
-        <h3>Active Tutors</h3>
-        <?php include "tutor-list.php"; ?>
-    </div>
-    <div class="pure-u-md-1-24"></div>
-    <div id="help-queue" class="pure-u-1 pure-u-md-1-4 card">
-        <a class="pure-button pure-button-primary" href="question-form.php">Ask for Help</a>
-        <h3>Help Queue <a href="."><i class="fa fa-refresh"></i></a></h3>
-        <?php include "question-list.php"; ?>
-    </div>
+<div class="pure-u-1 pure-u-md-1-4 card">
+    <h3>Active Tutors</h3>
+    <?php include "tutor-list.php"; ?>
+</div>
+<div class="pure-u-md-1-24"></div>
+<div id="help-queue" class="pure-u-1 pure-u-md-1-4 card">
+    <a class="pure-button pure-button-primary" href="question-form.php">Ask for Help</a>
+    <h3>Help Queue <a href="."><i class="fa fa-refresh"></i></a></h3>
+    <?php include "question-list.php"; ?>
+</div>
 </div>
 
 <script

77f5c5251c19598b6e059018df1d81d2084d92ef
Eric Dudley <ericdudley@email.com>
2017-09-22 19:35:00 -0400

    Secured init-db.

--- www/init-db.php
+++ www/init-db.php
@@ -1,8 +1,18 @@
 <?php
 include 'head.php';
-$db = SQLiteConnection::connect();
-if ($db != null) {
-    foreach (Config::CREATE_TABLE_STRINGS as $str) {
-        $db->exec($str);
+if($_POST['key'] == null){
+    echo 'No key provided!';
+    exit();
+} else{
+    if(hash('sha256', $_POST['key']) == '62f2a88a17dbe9403682a3195e76ba5473e3a04736ef795169e1e4b33246130d'){
+        $db = SQLiteConnection::connect();
+        if ($db != null) {
+            foreach (Config::CREATE_TABLE_STRINGS as $str) {
+                $db->exec($str);
+            }
+        }
+        echo 'Database was refreshed.';
+    } else{
+        echo 'incorrect key';
     }
 }
